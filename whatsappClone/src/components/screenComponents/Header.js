10078be47c58f3bd2eb7f2bcac2d769f678e7991
kodluyoruz <info@kodluyoruz.org>
2022-08-27 01:16:04 +0300

    comments and edit for Header.js

--- whatsappClone/src/components/screenComponents/Header.js
+++ whatsappClone/src/components/screenComponents/Header.js
@@ -2,21 +2,22 @@ import { View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native
 import React from 'react'
 import Feather from "react-native-vector-icons/Feather";
 
+// Arama çubuğu ve hemen altındaki başlıkların düzenlenmesi.
 const Header = () => {
   return (
     <View style={styles.container}>
       <View>
         <View style= {styles.search}>
             <Feather name="search" size={18}/>
-            <TextInput placeholder="Ara" style={{fontSize: 14, height: 35}}></TextInput>
+            <TextInput placeholder="Ara" style={styles.inputStyle}></TextInput>
         </View>
       </View>
       <View style= {styles.otherTitles}>
         <TouchableOpacity>
-          <Text style={{fontSize: 14, color:'#007AFF'}}>Toplu Mesaj Listeleri</Text>
+          <Text style={styles.textButton}>Toplu Mesaj Listeleri</Text>
         </TouchableOpacity>
         <TouchableOpacity>
-          <Text style={{fontSize: 14, color:'#007AFF'}}>Yeni Grup</Text>
+          <Text style={styles.textButton}>Yeni Grup</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -33,7 +34,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         textAlign: 'center',
         alignItems: 'center',
-        padding: 5,
+        padding: 6,
         margin: 5,
         backgroundColor: '#ededed',
         borderRadius: 8,      
@@ -43,6 +44,14 @@ const styles = StyleSheet.create({
         justifyContent:'space-between',
         padding: 10,
         color: 'blue'
+    },
+    inputStyle: {
+      fontSize: 14, 
+      height: 35
+    },
+    textButton: {
+      fontSize: 14, 
+      color:'#007AFF'
     }
 
 });

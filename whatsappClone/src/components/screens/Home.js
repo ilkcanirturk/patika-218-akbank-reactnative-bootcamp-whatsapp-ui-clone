a32e20a8f211224cf2ab7ed2192ac8b413afa133
kodluyoruz <info@kodluyoruz.org>
2022-08-26 14:14:45 +0300

    edit Home.js

--- whatsappClone/src/components/screens/Home.js
+++ whatsappClone/src/components/screens/Home.js
@@ -1,4 +1,4 @@
-import { View,  ScrollView, Button, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import { View,  ScrollView} from 'react-native';
 import React, { useLayoutEffect } from 'react';
 
 import ChatList from '../screenComponents/ChatList';
@@ -26,41 +26,6 @@ const Home = ({navigation}) => {
   )
 }
 
-// const styles = StyleSheet.create({
 
-//   container: { 
-//     display: "flex",
-//     flexDirection: "row",
-//     padding: 16,
-//     borderBottomWidth: 1,
-//     borderBottomColor: "rgba(0,0,0,0.3)",
-//   },
-//   pp: { 
-//     width: 64,
-//     height: 64,
-//     borderRadius: 32,
-//     backgroundColor: "rgba(0,0,0,0.3)",
-//     marginRight: 8
-//   },
-//   container2: {
-//     display: "flex",
-//     flexDirection: "column",
-//     flex: 1
-//   },
-//   container3: {
-//     display: "flex", 
-//     flexDirection: "row", 
-//     marginBottom: 8, 
-//     justifyContent:'space-between'
-//   },
-//   container4: {
-//     flexDirection: 'row'
-//   },
-//   text: {
-//     fontWeight: "600",
-//     fontSize: 16
-//   }
-
-// })
 
 export default Home;

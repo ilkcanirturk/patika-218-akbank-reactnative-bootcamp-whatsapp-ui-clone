8c4223e5cdcf8fb1a8e6a9833f9297acb9fef269
kodluyoruz <info@kodluyoruz.org>
2022-08-27 01:14:41 +0300

    add ChatRooms.ts

---
+++ whatsappClone/src/components/data/ChatRooms.ts
@@ -0,0 +1,113 @@
+export default [{
+    id: '1',
+    users: [{
+      id: 'u1',
+      name: 'User1',
+      imageUri: 'https://picsum.photos/id/277/200/300',
+    }, {
+      id: 'u2',
+      name: 'Zeynep Öz',
+      imageUri: 'https://picsum.photos/id/386/200/300',
+    }],
+    lastMessage: {
+      id: 'm1',
+      content: 'Yarın akşam saat 5 te alışveriş merkezinin hemen önündeki durakta bekleyeceğim.',
+      createdAt: '16:25',
+    }
+  }, {
+    id: '2',
+    users: [{
+      id: 'u1',
+      name: 'User1',
+      imageUri: 'https://picsum.photos/id/277/200/300',
+    }, {
+      id: 'u3',
+      name: 'Mert Bozok',
+      imageUri: 'https://picsum.photos/id/237/200/300',
+    }],
+    lastMessage: {
+      id: 'm2',
+      content: 'Tamamen unutmuşum.',
+      createdAt: '16:15',
+    }
+  }, {
+    id: '3',
+    users: [{
+      id: 'u1',
+      name: 'User1',
+      imageUri: 'https://picsum.photos/id/277/200/300',
+    }, {
+      id: 'u4',
+      name: 'Selin Gündüz',
+      imageUri: 'https://picsum.photos/id/167/200/300',
+    }],
+    lastMessage: {
+      id: 'm3',
+      content: 'Bugün hava çok güzel.',
+      createdAt: '15:46',
+    }
+  }, {
+    id: '4',
+    users: [{
+      id: 'u1',
+      name: 'User1',
+      imageUri: 'https://picsum.photos/id/277/200/300',
+    }, {
+      id: 'u5',
+      name: 'Mesut Karaca',
+      imageUri: 'https://picsum.photos/id/157/200/300',
+    }],
+    lastMessage: {
+      id: 'm4',
+      content: 'Tamamdır çok teşekkürler.',
+      createdAt: '15:30',
+    }
+  }, {
+    id: '5',
+    users: [{
+      id: 'u1',
+      name: 'User1',
+      imageUri: 'https://picsum.photos/id/277/200/300',
+    }, {
+      id: 'u6',
+      name: 'Berke Durmaz',
+      imageUri: 'https://picsum.photos/id/127/200/300',
+    }],
+    lastMessage: {
+      id: 'm5',
+      content: 'Yaz tatili için Kıbrıs çok iyi.',
+      createdAt: '15:12',
+    }
+  }, {
+    id: '6',
+    users: [{
+      id: 'u1',
+      name: 'User1',
+      imageUri: 'https://picsum.photos/id/277/200/300',
+    }, {
+      id: 'u7',
+      name: 'Banu Güler',
+      imageUri: 'https://picsum.photos/id/117/200/300',
+    }],
+    lastMessage: {
+      id: 'm6',
+      content: 'Anladım daha sonra görüşürüz.',
+      createdAt: '12:30',
+    }
+  }, {
+    id: '7',
+    users: [{
+      id: 'u1',
+      name: 'User1',
+      imageUri: 'https://picsum.photos/id/277/200/300',
+    }, {
+      id: 'u8',
+      name: 'Deniz Tatlı',
+      imageUri: 'https://picsum.photos/id/107/200/300',
+    }],
+    lastMessage: {
+      id: 'm7',
+      content: 'Kitabın son sayfası yırtılmış.',
+      createdAt: '11:42',
+    }
+  }]

2ef6994701bc86d87b78567e495d35b243ca4129
kodluyoruz <info@kodluyoruz.org>
2022-08-26 14:10:37 +0300

    initial commit

---
+++ whatsappClone/App.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/components/screens/Home';
+import SecondPage from './src/components/screens/SecondPage';
+import LogoHeader from './src/components/screenComponents/LogoHeader';
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <LogoHeader name="LogoHeader" component={LogoHeader}/>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen 
+          name="Homepage" 
+          component={Home}
+          options={{title: 'Sohbetler'}} 
+        />
+        <Stack.Screen 
+          name="Chat" 
+          component={SecondPage}
+          options={{title: 'Chat'}} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+export default App;

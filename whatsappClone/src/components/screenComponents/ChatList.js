8fdb0458c02131f7de369fb481f72c2df7c25d35
kodluyoruz <info@kodluyoruz.org>
2022-08-26 14:12:49 +0300

    add ChatList.js

---
+++ whatsappClone/src/components/screenComponents/ChatList.js
@@ -0,0 +1,31 @@
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as React from 'react';
+import Header from './Header';
+import ChatRoomItem from './ChatRoomItem';
+import chatRoomsData from '../data/ChatRooms';
+
+
+
+
+export default function ChatList() {
+  return (
+    <View style={styles.mainPage}>
+      <Header name="Header" component={Header}/>
+       <FlatList
+          data= {chatRoomsData}
+          renderItem={({ item }) => <ChatRoomItem chatRoom={item}/>}
+       />
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  mainPage:{
+    backgroundColor:'white',
+    flex: 1
+  }
+    
+  
+  })
